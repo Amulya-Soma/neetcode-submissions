849226047fe27131ef39ff33eb2f64f024e20f28
Amulya Soma <87309075+Amulya-Soma@users.noreply.github.com>
2026-08-17 20:32:51 +0530

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s_new = "".join(i.lower() for i in s if i.isalnum())
+        l = 0
+        r = len(s_new)-1
+        while(l<r):
+            if(s_new[l]!=s_new[r]):
+                return False
+            l+=1
+            r-=1
+        return True
